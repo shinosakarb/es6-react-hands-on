68459c2d67fffcf24e1c4b73ab348457edc7bb2a
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2016-06-08 22:06:49 +0900

    add props

--- App.js
+++ App.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 class App extends React.Component {
   render(){
-    return <div>Hello World</div>
+    return <div>Hello { `${this.props.name} #${this.props.number}` } </div>
   }
+}
+
+// デフォルト値をセット
+App.defaultProps = {
+  name: 'Shinosaka.rb'
+}
+
+// タイプを指定
+App.propTypes = {
+  // 文字列
+  name: React.PropTypes.string,
+  // 数値で必須
+  number: React.PropTypes.number.isRequired
 }
 
 export default App
